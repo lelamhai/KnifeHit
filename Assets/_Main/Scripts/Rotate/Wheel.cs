32045905c1f6b055f083fe33bc9441adeb83f4a0
lelamhai <lelamhai@ADMIN>
2023-07-01 18:39:42 +0700

    create list random rotate

--- Assets/_Main/Scripts/Rotate/Wheel.cs
+++ Assets/_Main/Scripts/Rotate/Wheel.cs
@@ -4,10 +4,45 @@ using UnityEngine;
 
 public class Wheel : MonoBehaviour
 {
-    //[SerializeField] private float _speed = 10f;
+    [SerializeField] private int _currentRotationElement = 0;
+    [SerializeField] private List<RotationElement> _listRotationElement = new List<RotationElement>();
 
-    private void Update()
+    [SerializeField] private bool _delay = true;
+    private float _speed = 0;
+    private float _duration = 0;
+
+    [System.Serializable]
+    private class RotationElement
     {
+        public float Speed;
+        public float Duration;
+    }
+
+
+    private void Start()
+    {
+        _currentRotationElement = Random.Range(0, _listRotationElement.Count - 1);
+        _speed = _listRotationElement[_currentRotationElement].Speed;
+        _duration = _listRotationElement[_currentRotationElement].Duration;
+    }
+
+    private void FixedUpdate()
+    {
+        this.transform.Rotate(new Vector3(0, 0, _speed * Time.fixedDeltaTime));
+
+        if (!_delay) return;
+        StartCoroutine(RandomElement());
+    }
+
+    private IEnumerator RandomElement()
+    {
+        _delay = false;
+        _currentRotationElement = Random.Range(0, _listRotationElement.Count);
+        _speed = _listRotationElement[_currentRotationElement].Speed;
+        Debug.Log("_speed: " + _speed);
+        _duration = _listRotationElement[_currentRotationElement].Duration;
+        yield return new WaitForSeconds(_duration);
+        _delay = true;
 
     }
 }
